adfb25b32ccc68d779bab86023593287626b7b06
Abu Taher <writerabutaher@gmail.com>
2023-07-22 18:17:12 +0600

    Update errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,6 @@
 const { constants } = require("../constants");
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode ? res.statusCode : 500;
+  const statusCode = res.statusCode || 500;
   switch (statusCode) {
     case constants.VALIDATION_ERROR:
       res.json({
